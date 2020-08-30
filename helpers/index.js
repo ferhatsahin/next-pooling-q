f1604818478ebfa333070b60559e5e1bffbb3dc3
ferhatsahin <ferhatsahin.ce@gmail.com>
2020-08-30 12:04:57 +0300

    add fetcher function to helpers

--- helpers/index.js
+++ helpers/index.js
@@ -1,5 +1,13 @@
 import moment from "moment";
 
+const baseURL = "https://polls.apiblueprint.org";
+
 export const dateFormatter = date => {
   return moment(new Date(date)).format("LL");
+};
+
+export const fetcher = async (endpoint,options) => {
+  const url = `${baseURL}${endpoint}`;
+  const response = await fetch(url, options)
+  return response.json()
 };
